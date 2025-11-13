76d355f8bc041e939822b2e1c18a9e01a04f481c
Bárbara <babimig230@gmail.com>
2025-11-13 02:36:23 -0300

    Correção Sonar

---
+++ src/test/java/com/bikeunirio/bicicletario/externo/exceptions/GlobalExceptionHandlerTest.java
@@ -0,0 +1,25 @@
+package com.bikeunirio.bicicletario.externo.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GlobalExceptionHandlerTest {
+    /*
+
+    @Test
+    void handleValidationException() {
+    }
+
+    @Test
+    void handleObjetoNaoEncontrado() {
+    }
+
+    @Test
+    void handleMailException() {
+    }
+
+    @Test
+    void testHandleMailException() {
+    }*/
+}
--- src/test/java/com/bikeunirio/bicicletario/externo/service/EmailServiceTest.java
+++ src/test/java/com/bikeunirio/bicicletario/externo/service/EmailServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
