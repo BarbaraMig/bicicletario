5cd51e90f159c4822b43b4b0e0b32a3188401aa8
Bárbara <babimig230@gmail.com>
2025-11-13 00:55:37 -0300

    EmailServiceTest atualização

--- src/test/java/com/bikeunirio/bicicletario/externo/service/EmailServiceTest.java
+++ src/test/java/com/bikeunirio/bicicletario/externo/service/EmailServiceTest.java
@@ -6,9 +6,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
+import org.springframework.mail.MailParseException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -25,6 +25,10 @@ public class EmailServiceTest {
     @Test
     public void testEnviarEmailCerto(){
         EmailDto dto = new EmailDto("emailexternoes2@gmail.com","assunto","mensagem");
+
+        //mock do mailSender
+        doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
         ResponseEntity<?> respostaService = emailService.enviarEmail(dto);
 
         //aqui ele verifica se o código e o corpo da requisição são iguais
@@ -35,27 +39,35 @@ public class EmailServiceTest {
     }
     //O email é inválido
     @Test
-    public void testEnviarEmailErradoInvalido(){
+    public void testEnviarEmailInvalido(){
         EmailDto dto = new EmailDto("receptor","assunto","mensagem");
+
+        //mock do mailSender
+        doThrow(new MailParseException("Erro no envio"){}).when(mailSender)
+                .send(any(SimpleMailMessage.class));
+
         ResponseEntity<?> respostaService = emailService.enviarEmail(dto);
 
         //aqui ele verifica se o código e o corpo da requisição são iguais
         assertEquals(422, respostaService.getStatusCode().value());
         assertEquals(dto,respostaService.getBody());
+
         //verifica se o mailSender só foi chamado uma vez
         verify(mailSender, times(1)).send(any(SimpleMailMessage.class));
     }
+
     //O email não existe
     //https://captainverify.com/pt/mail-tester.html descobre se o email existe ou não
     @Test
-    public void testEnviarEmailErradoExistencia(){
+    public void testEnviarEmailNaoExistente(){
         EmailDto dto = new EmailDto("ahivbvlaihvu@gmail.com","assunto","mensagem");
-        ResponseEntity<?> respostaService = emailService.enviarEmail(dto);
 
         //simplificação: lance uma exceção MailException quando o mailSender enviar uma mensagem
         //da classe SimpleMailMessage (SMM)
         doThrow(new MailException("Erro no envio"){}).when(mailSender)
                 .send(any(SimpleMailMessage.class));
+
+        ResponseEntity<?> respostaService = emailService.enviarEmail(dto);
 
         //aqui ele verifica se o código e o corpo da requisição são iguais
         assertEquals(422, respostaService.getStatusCode().value());
