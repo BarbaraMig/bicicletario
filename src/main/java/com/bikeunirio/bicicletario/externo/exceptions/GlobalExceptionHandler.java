b16196920ecdb4acc162ba03238f84f250020dcc
BarbaraMig <babimig230@gmail.com>
2025-11-17 10:34:57 -0300

    Correção imports

--- src/main/java/com/bikeunirio/bicicletario/externo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bikeunirio/bicicletario/externo/exceptions/GlobalExceptionHandler.java
@@ -1,43 +1,25 @@
 package com.bikeunirio.bicicletario.externo.exceptions;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailParseException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler{
-    private final String status = "status";
-    private final String message = "mensagem";
+    private static final String STATUS = "status";
+    private static final String MESSAGE = "mensagem";
 
-    //Exceptions do Email
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<String>> handleValidationException(MethodArgumentNotValidException exception){
-        List<String> erros = exception.getBindingResult()
-                .getAllErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .toList();
-
-        return ResponseEntity
-                .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body(erros);
-    }
 
     @ExceptionHandler(MailException.class)
     public ResponseEntity<Map<String,Object>> handleMailException(MailException exception){
         Map<String, Object> corpoDoErro = new LinkedHashMap<>();
-        corpoDoErro.put(status, HttpStatus.INTERNAL_SERVER_ERROR.value());
-        corpoDoErro.put(message, exception.getMessage());
+        corpoDoErro.put(STATUS, HttpStatus.INTERNAL_SERVER_ERROR.value());
+        corpoDoErro.put(MESSAGE, exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(corpoDoErro);
     }
 
@@ -45,8 +27,8 @@ public class GlobalExceptionHandler{
     @ExceptionHandler(MailParseException.class)
     public ResponseEntity<Map<String,Object>> handleMailException(MailParseException exception){
         Map<String, Object> corpoDoErro = new LinkedHashMap<>();
-        corpoDoErro.put(status, HttpStatus.UNPROCESSABLE_ENTITY.value());
-        corpoDoErro.put(message, exception.getMessage());
+        corpoDoErro.put(STATUS, HttpStatus.UNPROCESSABLE_ENTITY.value());
+        corpoDoErro.put(MESSAGE, exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(corpoDoErro);
     }
 
