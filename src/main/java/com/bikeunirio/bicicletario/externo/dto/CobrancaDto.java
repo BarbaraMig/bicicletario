9e5837c7ed7d3338a023c7e4dd1e931fb6135005
Bárbara <babimig230@gmail.com>
2025-12-10 16:39:39 -0300

    Update

--- src/main/java/com/bikeunirio/bicicletario/externo/dto/CobrancaDto.java
+++ src/main/java/com/bikeunirio/bicicletario/externo/dto/CobrancaDto.java
@@ -1,7 +1,10 @@
 package com.bikeunirio.bicicletario.externo.dto;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class CobrancaDto {
     private Long idCobranca;
     private String idApi;
@@ -20,51 +23,5 @@ public class CobrancaDto {
     private Float valorCobranca;
     private Long idCiclista;
 
-    public Long getIdCobranca() {
-        return idCobranca;
-    }
 
-    public void setIdCobranca(Long idCobranca) {
-        this.idCobranca = idCobranca;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getHoraSolicitacao() {
-        return horaSolicitacao;
-    }
-
-    public void setHoraSolicitacao(LocalDateTime horaSolicitacao) {
-        this.horaSolicitacao = horaSolicitacao;
-    }
-
-    public LocalDateTime getHoraFinalizacao() {
-        return horaFinalizacao;
-    }
-
-    public void setHoraFinalizacao(LocalDateTime horaFinalizacao) {
-        this.horaFinalizacao = horaFinalizacao;
-    }
-
-    public Float getValorCobranca() {
-        return valorCobranca;
-    }
-
-    public void setValorCobranca(Float valorCobranca) {
-        this.valorCobranca = valorCobranca;
-    }
-
-    public Long getIdCiclista() {
-        return idCiclista;
-    }
-
-    public void setIdCiclista(Long idCiclista) {
-        this.idCiclista = idCiclista;
-    }
 }
--- src/main/java/com/bikeunirio/bicicletario/externo/entity/FilaCobranca.java
+++ src/main/java/com/bikeunirio/bicicletario/externo/entity/FilaCobranca.java
@@ -5,8 +5,23 @@ import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
 public class FilaCobranca {
     List<Cobranca> filaCobrancas;
     LocalDateTime ultimaChecagem;
+
+    public List<Cobranca> getFilaCobrancas() {
+        return filaCobrancas;
+    }
+
+    public void setFilaCobrancas(List<Cobranca> filaCobrancas) {
+        this.filaCobrancas = filaCobrancas;
+    }
+
+    public LocalDateTime getUltimaChecagem() {
+        return ultimaChecagem;
+    }
+
+    public void setUltimaChecagem(LocalDateTime ultimaChecagem) {
+        this.ultimaChecagem = ultimaChecagem;
+    }
 }
